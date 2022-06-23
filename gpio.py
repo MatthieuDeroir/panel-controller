b8115f452777a8dea0140201250277ccac5643cd
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-06-23 09:49:27 +0200

    led

--- gpio.py
+++ gpio.py
@@ -14,8 +14,8 @@ led_2_index = 27
 GPIO.setup(door_1_index, GPIO.IN)
 GPIO.setup(door_2_index, GPIO.IN)
 GPIO.setup(power_index, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(led_1_index, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(led_2_index, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(led_1_index, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(led_2_index, GPIO.OUT, initial=GPIO.HIGH)
 
 
 # storing results
@@ -27,16 +27,16 @@ def update_input():
 
 def change_output(state):
     if state:
-        led_1 = GPIO.output(led_1_index, GPIO.HIGH)
-        led_2 = GPIO.output(led_2_index, GPIO.HIGH)
+        led_1 = GPIO.output(led_1_index, GPIO.LOW)
+        led_2 = GPIO.output(led_2_index, GPIO.LOW)
         state_1 = GPIO.gpio_function(led_1_index)
         state_2 = GPIO.gpio_function(led_2_index)
         print("ALLUMAGE")
         print("LED 1 :", led_1, state_1)
         print("LED 2 :", led_2, state_2)
     else:
-        led_1 = GPIO.output(led_1_index, GPIO.LOW)
-        led_2 = GPIO.output(led_2_index, GPIO.LOW)
+        led_1 = GPIO.output(led_1_index, GPIO.HIGH)
+        led_2 = GPIO.output(led_2_index, GPIO.HIGH)
         state_1 = GPIO.gpio_function(led_1_index)
         state_2 = GPIO.gpio_function(led_2_index)
         print("ETEIGNAGE")
